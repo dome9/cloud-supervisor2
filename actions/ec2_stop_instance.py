85269353135f3d13a3b739b04815c80b9fa71a67
Alex Corstorphine <alex@abcwebdesigner.com>
2018-03-15 16:17:42 -0700

    cleaned up boto session

--- actions/ec2_stop_instance.py
+++ actions/ec2_stop_instance.py
@@ -1,10 +1,10 @@
 import boto3
 
 ### Turn off EC2 instance ###
-def run_action(boto_connections,rule,entity,params):
+def run_action(boto_session,rule,entity,params):
     instance = entity['id']
-
-    result = boto_connections['ec2_client'].stop_instances(InstanceIds=[instance])
+    ec2 = boto_session.client('ec2')
+    result = ec2.stop_instances(InstanceIds=[instance])
 
     responseCode = result['ResponseMetadata']['HTTPStatusCode']
     if responseCode >= 400:
--- handle_event.py
+++ handle_event.py
@@ -7,35 +7,6 @@ from botocore.exceptions import ClientError
 account_mode = os.getenv('ACCOUNT_MODE','')
 cross_account_role_name = os.getenv('CROSS_ACCOUNT_ROLE_NAME','')
 
-#### NEED TO CLEAN UP
-# http://boto3.readthedocs.io/en/latest/reference/core/session.html
-def create_boto_connections(credentials_for_event,region):
-    boto_connections = {}
-    
-    if credentials_for_event is "not_needed":
-        boto_connections['ec2_client'] = boto3.client('ec2', region_name=region)
-        boto_connections['ec2_resource'] = boto3.resource('ec2', region_name=region)
-        boto_connections['s3_client'] = boto3.client('s3')
-
-    else:
-        boto_connections['ec2_client'] = boto3.client('ec2', region_name=region,         
-            aws_access_key_id = credentials_for_event['AccessKeyId'],
-            aws_secret_access_key = credentials_for_event['SecretAccessKey'],
-            aws_session_token = credentials_for_event['SessionToken']
-            )
-        boto_connections['ec2_resource'] = boto3.resource('ec2', region_name=region,         
-            aws_access_key_id = credentials_for_event['AccessKeyId'],
-            aws_secret_access_key = credentials_for_event['SecretAccessKey'],
-            aws_session_token = credentials_for_event['SessionToken']
-            )
-        boto_connections['s3_client'] = boto3.client('s3',         
-            aws_access_key_id = credentials_for_event['AccessKeyId'],
-            aws_secret_access_key = credentials_for_event['SecretAccessKey'],
-            aws_session_token = credentials_for_event['SessionToken']
-            )
-
-    return boto_connections    
-
 def handle_event(message,text_output_array):
     post_to_sns = True
     #Break out the values from the JSON payload from Dome9
@@ -100,22 +71,26 @@ def handle_event(message,text_output_array):
                     #get the accountID
                     sts = boto3.client("sts")
                     lambda_account_id = sts.get_caller_identity()["Account"]
+
                 except ClientError as e:
                     text_output_array.append("Unexpected STS error: %s \n"  % e)
 
+
                 #Account mode will be set in the lambda variables. We'll default to single mdoe 
-                if lambda_account_id != event_account_id: #Work needs to be done outside of this account
+                if lambda_account_id != event_account_id: #The remediation needs to be done outside of this account
                     if account_mode == "multi": #multi or single account mode?
                         #If it's not the same account, try to assume role to the new one
-                        if cross_account_role_name:
+                        if cross_account_role_name: # This allows users to set their own role name if they have a different naming convention they have to follow
                             role_arn = "arn:aws:iam::" + event_account_id + ":role/" + cross_account_role_name
                         else:
                             role_arn = "arn:aws:iam::" + event_account_id + ":role/dome9-auto-remediations"
 
                         text_output_array.append("Compliance failure was found for an account outside of the one the function is running in. Trying to assume_role to target account %s .\n" % event_account_id) 
+
                         try:
                             credentials_for_event = globals()['all_session_credentials'][account_id]
-                            text_output_array.append("Found existing credentials to use from still warm lambda functions. Skipping another STS assume role\n")                
+                            text_output_array.append("Found existing credentials to use from still warm lambda functions. Skipping another STS assume role\n")        
+
                         except (NameError,KeyError):
                             #If we can't find the credentials, try to generate new ones
                             text_output_array.append("Session credentials weren't found cached in the function. Trying to generate new ones.\n")
@@ -134,6 +109,13 @@ def handle_event(message,text_output_array):
                                 # From the response that contains the assumed role, get the temporary credentials that can be used to make subsequent API calls
                                 credentials_for_event = all_session_credentials[event_account_id] = assumedRoleObject['Credentials']
                                 
+                                boto_session = boto3.Session(
+                                    region_name=region,         
+                                    aws_access_key_id = credentials_for_event['AccessKeyId'],
+                                    aws_secret_access_key = credentials_for_event['SecretAccessKey'],
+                                    aws_session_token = credentials_for_event['SessionToken']
+                                    )
+
                             except ClientError as e:
                                 error = e.response['Error']['Code']
                                 print(e)
@@ -142,21 +124,18 @@ def handle_event(message,text_output_array):
                                     continue                          
 
                     else:
+                        # In single account mode, we don't want to try to run actions outside of this one
                         text_output_array.append("Error: This finding was found in account id %s. The Lambda function is running in account id: %s. Remediations need to be ran from the account there is the issue in.\n" % (event_account_id, lambda_account_id))
                         post_to_sns = False
                         return text_output_array,post_to_sns
-                                
-                else:
-                    credentials_for_event = "not_needed"
 
-                #Generate boto_connections to pass to functions and do it without assume role credentials
-                try:
-                    boto_connections = create_boto_connections(credentials_for_event,region)        
-                except Exception as e: 
-                    text_output_array.append("Error while creating boto_connections\n")
+                else:
+                    #Boto will default to default session if we don't need assume_role credentials
+                    boto_session = boto3.Session(region_name=region)                     
 
                 ## Run the action
-                action_msg = action_module.run_action(boto_connections,message['rule'],message['entity'],params)
+                action_msg = action_module.run_action(boto_session,message['rule'],message['entity'],params)
+
             except Exception as e: 
                 action_msg = "Error while executing function '%s'.\n Error: %s \n" % (action,e)
                 print(action_msg)
